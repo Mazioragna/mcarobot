657862a68107cb536e169128b452acc6804f9ab6
Mazioragna <93527813+Mazioragna@users.noreply.github.com>
2021-11-28 15:11:25 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,12 +30,11 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [Naughty PROFESSOR](https://t.me/movie_club_arena)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Tutorial : [With Love ♥️✨](https://t.me/movie_club_arena)
 """
